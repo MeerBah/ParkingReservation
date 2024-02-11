d2b7d093ef1ee33fcf223a8e19de6fc9d357a516
ameerbahrin <ameerbahrin16@gmail.com>
2024-02-12 00:53:54 +0800

    bug

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(ParkingSeeder::class);
+        $this->call(ParkingSizeSeeder::class);
+        $this->call(ParkingTimeSeeder::class);
+        $this->call(StatusSeeder::class);
+
     }
 }
